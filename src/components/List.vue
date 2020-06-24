7dc4b045c30cc15884ef0178bce70f82870b1684
Alba Cuatecontzi <isa081398@gmail.com>
2020-06-24 18:05:06 -0500

    editar vuedjango

---
+++ src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        
+        <div class="box" v-for="e in elements" v-bind:key="e">
+            {{ e }}
+        </div>
+
+        <button v-on:click="findAll">Click</button>
+
+        {{ msj }}
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msj: "... :(",
+            elements: ["Uno", "Dos", "Tres"]
+        };
+    },
+    methods: {
+        findAll: function(){
+            console.log("Hola Mundo")
+            this.msj = "Click :)"
+        }
+    },
+}
+</script>
