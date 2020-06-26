0a38570a0320625f9bf7049d5cee65fd9ee1d933
Alba Cuatecontzi <isa081398@gmail.com>
2020-06-26 00:36:25 -0500

    editar vuedjango

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -2,15 +2,19 @@
     <div>
         <b-card>
             <h1> {{ element.title }} </h1>
-            <div class="p-3">
+            <div class="p-3" v-if="element.category">
+                <router-link class="btn btn-danger btn-sm" :to="{ name: 'list-category', params:{id: element.category} }">{{category.title}}</router-link>
+                <router-link class="ml-2 btn btn-danger btn-sm" :to="{ name: 'list-type', params:{id: element.type} }">{{type.title}}</router-link>
             <b-card-text>{{ element.description }}</b-card-text>
             </div>
         </b-card>
-
     </div>
 </template>
 
 <script>
+
+const axios = require('axios')
+
 export default {
 
     created(){
@@ -19,15 +23,31 @@ export default {
     },
     data(){
         return{
-            element: Object
+            element: Object,
+            category: Object,
+            type: Object
         };
     },
     methods: {
         find: function() {
             fetch("http://127.0.0.1:8000/api/element/"+this.$route.params.id+"/?format=json")
             .then(res => res.json())
-            .then(res => (this.element = res));
+            .then(res => {
+                this.element = res;
+                this.findCategory (this.element.category);
+                this.findType (this.element.type);
+            });
+        },
+        findCategory: function(id) {
+            axios.get("http://127.0.0.1:8000/api/category/" + id + "/?format=json")
+            //.then(res => res.json())
+            .then(res => (this.category = res.data));
+        },
+        findType: function(id) {
+            axios.get("http://127.0.0.1:8000/api/type/" + id + "/?format=json")
+            //.then(res => res.json())
+            .then(res => (this.type = res.data));
         }
-    },
+    }
 };
 </script>
